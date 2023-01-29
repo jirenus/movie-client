7e9408588e91a6735eaf6850a8ef78fba7a53586
jirenus781 <ngotuyen781@gmail.com>
2023-01-29 11:44:46 +0700

    feat: add homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
 import api from './api/axiosConfig';
+import { useEffect, useState } from 'react';
+import Layout from './components/Layout';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 function App() {
 
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const res = await api.get('/api/v1/movies');
+      console.log(res.data.data);
+      
+      setMovies(res.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  
+
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home movies={movies} />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
